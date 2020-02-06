bb08e106f08bfaa3451804db341f6eab1214158c
Glitch (deluxe-protocol-g5dhxtm58m) <none>
2020-02-06 19:31:09 +0000

    clear everything and rewrite base code

--- commands/docs.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'docs',
-	description: 'Gives a link to the official Mazz documentation.',
-	aliases: ['guide'],
-	execute(message) {
-		message.channel.send(`:ping_pong: Pong! Returned at ${Math.round(message.client.ping)}ms.`);
-	},
-};
--- commands/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'ping',
-	description: 'Checks the bot\'s ping.',
-	execute(message) {
-		message.channel.send(`:ping_pong: Pong! Returned at ${Math.round(message.client.ping)}ms.`);
-	},
-};
