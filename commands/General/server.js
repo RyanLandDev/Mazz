8444360f49c9bff1945d6d899684c5becf603cd3
Intervexian <61286305+Intervexian@users.noreply.github.com>
2020-02-20 23:41:26 +0000

    [M] I had to change the Aliases for Server.js

--- commands/General/server.js
+++ commands/General/server.js
@@ -8,7 +8,7 @@ module.exports = class extends Command {
     super(...args, {
       description: 'Links to the Mazz documentation.',
       guarded: true,
-      aliases: ['support', 'supportserver', 'discordserver', 'guild']
+      aliases: ['support', 'supportserver', 'discordserver', 'guild'],
     });
   }
 
