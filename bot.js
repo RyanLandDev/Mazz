4363b52334809d4482d9632981cce396ced1e944
RyanLandDev <ryantuijp@hotmail.com>
2020-03-16 12:54:03 +0100

    [M] Small fix

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ const http = require('http');
 const express = require('express');
 const app = express();
 app.get('/', (request, response) => {
-  console.log(Date.now() + 'Ping Received');
+  console.log(Date.now() + ' Ping Received');
   response.sendStatus(200);
 });
 app.listen(process.env.PORT);
