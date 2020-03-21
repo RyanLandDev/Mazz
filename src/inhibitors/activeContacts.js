98f6653b48f3760f5a46e1e69a7cdf886a578586
RyanLandDev <ryantuijp@hotmail.com>
2020-03-21 20:37:10 +0100

    [M] Contacts fix

--- src/inhibitors/activeContacts.js
+++ src/inhibitors/activeContacts.js
@@ -17,6 +17,7 @@ module.exports = class extends Inhibitor {
     for (let i = 0; i < activeContacts.length; i++) {
       let contact;
       for (let i2 = 1; i2 < contacts.length; i2++) if (contacts[i2].codename === activeContacts[i]) contact = contacts[i2];
+      if (!contact.timeKey) continue;
       const nowUnix = parseInt(moment().format('x'));
       const initUnix = parseInt(message.author.settings.get(contact.timeKey));
       if ((nowUnix - initUnix) > contact.temporaryTime) message.author.settings.update('activeContacts', contact.codename, { action: 'remove' });
