e47878d81433b389ccc0f87a5cf34a09413cb31c
RyanLandDev <ryantuijp@hotmail.com>
2020-03-22 10:10:11 +0100

    [M] Set level command finish

---
+++ src/commands/Mazz Staff/Economy Moderation/setlevel.js
@@ -0,0 +1,21 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+  constructor(...args) {
+    super(...args, {
+      description: 'Set the level of a user. Use the flag \'-add\' if you want to add on to the level instead.',
+      guarded: true,
+      usage: '<member:user> <level:num> [-add:literal]',
+      usageDelim: ' ',
+      // mazz admin
+      permissionLevel: 28,
+    });
+  }
+
+  async run(msg, params) {
+    params[0].settings.update('level', params[2] ? params[0].settings.get('level') + params[1] : params[1]);
+    msg.send(`The level of \`${params[0].username} (${params[0].id})\` has been set to ${msg.guild.settings.get('level')}**${params[2] ? params[0].settings.get('level') + params[1] : params[1]}**.`);
+  }
+
+};
