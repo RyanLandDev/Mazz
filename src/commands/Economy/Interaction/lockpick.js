ea08443efc125d8a43128e5b991b05e00d94deca
RyanLandDev <ryantuijp@hotmail.com>
2020-03-23 17:33:36 +0100

    [M] Package.json update + Continue lockpick

--- src/commands/Economy/Interaction/lockpick.js
+++ src/commands/Economy/Interaction/lockpick.js
@@ -8,19 +8,18 @@ module.exports = class extends Command {
       usage: '<member:user>',
       cooldown: 9000,
       runIn: ['text'],
-      enabled: false,
     });
   }
 
   async run(msg, params) {
     const user = params[0];
     msg.delete();
+    if (user === msg.author) throw msg.send('You can\'t lockpick yourself!');
     const start = parseInt(moment().format('x'));
-    const lockpick = [{}];
+    const lockpick = [];
+    for (let i = 0; i < 5; i++) for (let j = 0; j < 7; j++) lockpick.push({ 'name': 'square', 'emoji': ':black_large_square:', 'row': i, 'column': j });
+    console.log(lockpick);
     msg.author.send('**Lockpicking in progress** | Starting')
       .catch(() => msg.send('You have your DMs disabled, please enable them to be able to lockpick'));
-
-    Update() {
-    }
   }
 };
