f86e5571838ff6220eeb6fc291bb117847cf4724
RyanLandDev <46247385+RyanLandDev@users.noreply.github.com>
2020-10-30 09:17:27 +0100

    Create activeItems.js

---
+++ src/inhibitors/activeItems.js
@@ -0,0 +1,22 @@
+const { Inhibitor } = require('klasa');
+const items = require('../config/items/inv_items.json');
+
+module.exports = class extends Inhibitor {
+  constructor(...args) {
+    super(...args, {});
+  }
+
+  async run(message, command) {
+    if (command.category !== 'Economy') return;
+
+    const activeItems = message.author.settings.get('activeItems').slice();
+    if (activeItems.length <= 0) return;
+    for (let i = 0; i < activeItems.length; i++) {
+      let item;
+      for (let i2 = 1; i2 < items.length; i2++) if (items[i2].codename === activeItems[i]) item = items[i2];
+      const nowUnix = Date.now();
+      const initUnix = parseInt(message.author.settings.get(item.statistics.key));
+      if ((nowUnix - initUnix) > item.temporaryTime) message.author.settings.update('activeItems', item.codename, { arrayAction: 'remove' });
+    }
+  }
+};
